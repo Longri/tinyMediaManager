ba3d840933b6b5aefb8d0f97e4016e87cb9fe1d6
Longri <www.longri.de@googlemail.com>
2018-05-03 12:20:34 +0200

    add own module for changes

---
+++ longri/src/main/java/de/longri/tinymediamanager/ChangedMainWindow.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2018 team-cachebox.de
+ *
+ * Licensed under the : GNU General Public License (GPL);
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.gnu.org/licenses/gpl.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.longri.tinymediamanager;
+
+import org.tinymediamanager.ui.MainWindow;
+
+/**
+ * Created by Longri on 03.05.2018.
+ */
+public class ChangedMainWindow extends MainWindow {
+  /**
+   * Create the application.
+   *
+   * @param name the name
+   */
+  public ChangedMainWindow(String name) {
+    super(name);
+  }
+}
